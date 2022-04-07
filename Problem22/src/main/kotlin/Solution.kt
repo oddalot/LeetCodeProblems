6842d5603991176442cb230ea5e3e7c5cf3998d3
oddalot <oddalot@yahoo.com>
2022-04-06 19:17:55 -0600

    add problem 22

--- Problem22/src/main/kotlin/Solution.kt
+++ Problem22/src/main/kotlin/Solution.kt
@@ -1,2 +1,29 @@
 class Solution {
+    fun generateParenthesis(n: Int): List<String> {
+        if (n == 0) return emptyList()
+        var parenSet = mutableSetOf<String>("()")
+        if (n == 1) return parenSet.toList()
+        (2..n).forEach { _ ->
+            val newParenSet = mutableSetOf<String>()
+            parenSet.forEach { parens->
+                newParenSet.addAll(getParens(parens))
+            }
+            parenSet = newParenSet
+        }
+
+        return parenSet.toList()
+    }
+
+    private fun getParens(parens: String): Set<String> {
+        val parenSet = mutableSetOf<String>()
+        (0..parens.length).forEach { i ->
+            parenSet.add(parens.insert(i, "()"))
+        }
+
+        return parenSet
+    }
+
+    private fun String.insert(index: Int, string: String): String {
+        return this.substring(0, index) + string + this.substring(index, this.length)
+    }
 }
--- Problem22/src/test/kotlin/Test.kt
+++ Problem22/src/test/kotlin/Test.kt
@@ -1,3 +1,16 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 class Test {
     private val solution = Solution()
+
+    @Test
+    fun `generates all combinations of parens`() {
+        val n = 2
+        val expectedParens = listOf(
+            "()()",
+            "(())"
+        )
+        assertEquals(expectedParens, solution.generateParenthesis(n))
+    }
 }
