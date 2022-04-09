5e5967fd0a72eff0f07f3a826f005399e37716af
oddalot <oddalot@yahoo.com>
2022-04-08 20:09:56 -0600

    add problem 23

--- Problem23/src/main/kotlin/Solution.kt
+++ Problem23/src/main/kotlin/Solution.kt
@@ -1,2 +1,39 @@
 class Solution {
+    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+        var head: ListNode? = null
+        var tail: ListNode? = null
+
+        var isNotEmpty = true
+
+        while (isNotEmpty) {
+            var localIsNotEmpty = false
+
+            var leastElement = Pair(-1, Int.MAX_VALUE)
+            lists.forEachIndexed { index, list ->
+                if (list != null) localIsNotEmpty = true
+
+                val first = list?.`val`
+                if (first != null) {
+                    if (first < leastElement.second) {
+                        leastElement = Pair(index, first)
+                    }
+                }
+            }
+
+            if (localIsNotEmpty) {
+                if (tail == null) {
+                    head = ListNode(leastElement.second)
+                    tail = head
+                } else {
+                    tail.next = ListNode(leastElement.second)
+                    tail = tail.next
+                }
+                lists[leastElement.first] = lists[leastElement.first]?.next
+            }
+
+            isNotEmpty = localIsNotEmpty
+        }
+
+        return head
+    }
 }
--- Problem23/src/test/kotlin/Test.kt
+++ Problem23/src/test/kotlin/Test.kt
@@ -1,3 +1,47 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 class Test {
     private val solution = Solution()
+
+    @Test
+    fun `merges 3 linked lists successfully`() {
+        val nodeOne1 = ListNode(3)
+        val nodeOne2 = ListNode(5)
+        val nodeOne3 = ListNode(6)
+        nodeOne1.next = nodeOne2
+        nodeOne2.next = nodeOne3
+
+        val nodeTwo1 = ListNode(4)
+        val nodeTwo2 = ListNode(7)
+        nodeTwo1.next = nodeTwo2
+
+        val nodeThree1 = ListNode(2)
+        val nodeThree2 = ListNode(8)
+        nodeThree1.next = nodeThree2
+
+        val outputNode1 = ListNode(2)
+        val outputNode2 = ListNode(3)
+        val outputNode3 = ListNode(4)
+        val outputNode4 = ListNode(5)
+        val outputNode5 = ListNode(6)
+        val outputNode6 = ListNode(7)
+        val outputNode7 = ListNode(8)
+        outputNode1.next = outputNode2
+        outputNode2.next = outputNode3
+        outputNode3.next = outputNode4
+        outputNode4.next = outputNode5
+        outputNode5.next = outputNode6
+        outputNode6.next = outputNode7
+
+        var head = solution.mergeKLists(arrayOf(nodeOne1, nodeTwo1, nodeThree1))
+        var outputHead: ListNode? = outputNode1
+
+        while (head != null) {
+            assertEquals(outputHead?.`val`, head.`val`)
+
+            head = head.next
+            outputHead = outputHead?.next
+        }
+    }
 }
