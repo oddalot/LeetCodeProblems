fbbf8c8287a613b731274904b6bd93c67b844f4e
oddalot <oddalot@yahoo.com>
2022-07-09 13:49:57 -0600

    getting row, column, box, board behavior more defined

--- Problem37/src/main/kotlin/Board.kt
+++ Problem37/src/main/kotlin/Board.kt
@@ -1,30 +1,34 @@
-class Board {
-    private val cells = Array(81) { location -> Cell(Value.EMPTY, location) }
+class Board(board: Array<CharArray>) {
+    private val cells = Array(81) { position -> Cell(position = position) }
     private val rows = Array(9) { columnIndex ->
         val columnScale = columnIndex * 9
-        arrayOf(
-            cells[0 + columnScale],
-            cells[1 + columnScale],
-            cells[2 + columnScale],
-            cells[3 + columnScale],
-            cells[4 + columnScale],
-            cells[5 + columnScale],
-            cells[6 + columnScale],
-            cells[7 + columnScale],
-            cells[8 + columnScale]
+        ValueSet(
+            arrayOf(
+                cells[0 + columnScale],
+                cells[1 + columnScale],
+                cells[2 + columnScale],
+                cells[3 + columnScale],
+                cells[4 + columnScale],
+                cells[5 + columnScale],
+                cells[6 + columnScale],
+                cells[7 + columnScale],
+                cells[8 + columnScale]
+            )
         )
     }
     private val columns = Array(9) { rowIndex ->
-        arrayOf(
-            cells[rowIndex],
-            cells[rowIndex + (9 * 1)],
-            cells[rowIndex + (9 * 2)],
-            cells[rowIndex + (9 * 3)],
-            cells[rowIndex + (9 * 4)],
-            cells[rowIndex + (9 * 5)],
-            cells[rowIndex + (9 * 6)],
-            cells[rowIndex + (9 * 7)],
-            cells[rowIndex + (9 * 8)]
+        ValueSet(
+            arrayOf(
+                cells[rowIndex],
+                cells[rowIndex + (9 * 1)],
+                cells[rowIndex + (9 * 2)],
+                cells[rowIndex + (9 * 3)],
+                cells[rowIndex + (9 * 4)],
+                cells[rowIndex + (9 * 5)],
+                cells[rowIndex + (9 * 6)],
+                cells[rowIndex + (9 * 7)],
+                cells[rowIndex + (9 * 8)]
+            )
         )
     }
     private val boxes = Array(9) { boxIndex ->
@@ -34,42 +38,53 @@ class Board {
         // 27, 28, 29, 36, 37, 38, 45, 46, 47
         val rowScale = ((boxIndex % 3) * 3) + (27 * (boxIndex / 3))
 
-        arrayOf(
-            cells[0 + rowScale],
-            cells[1 + rowScale],
-            cells[2 + rowScale],
-            cells[9 + rowScale],
-            cells[10 + rowScale],
-            cells[11 + rowScale],
-            cells[18 + rowScale],
-            cells[19 + rowScale],
-            cells[20 + rowScale]
+        ValueSet(
+            arrayOf(
+                cells[0 + rowScale],
+                cells[1 + rowScale],
+                cells[2 + rowScale],
+                cells[9 + rowScale],
+                cells[10 + rowScale],
+                cells[11 + rowScale],
+                cells[18 + rowScale],
+                cells[19 + rowScale],
+                cells[20 + rowScale]
+            )
         )
+    }
+
+    init {
+        var i = 0
+        board.forEach { row ->
+            row.forEach { cellChar ->
+                this.setCellValue(i++, cellChar.toValue())
+            }
+        }
     }
 
     fun printBoard() {
         cells.forEach { cell ->
-            println("cell: ${cell.location}, ${cell.value}")
+            println("cell: ${cell.position}, ${cell.value}")
         }
 
         rows.forEach { row ->
             println("row: $row")
-            row.forEach { cell ->
-                println("row cell: ${cell.location}, ${cell.value}")
+            row.cells.forEach { cell ->
+                println("row cell: ${cell.position}, ${cell.value}")
             }
         }
 
         columns.forEach { column ->
             println("column: $column")
-            column.forEach { cell ->
-                println("column cell: ${cell.location}, ${cell.value}")
+            column.cells.forEach { cell ->
+                println("column cell: ${cell.position}, ${cell.value}")
             }
         }
 
         boxes.forEach { box ->
             println("boxes: $boxes")
-            box.forEach { cell ->
-                println("box cell: ${cell.location}, ${cell.value}")
+            box.cells.forEach { cell ->
+                println("box cell: ${cell.position}, ${cell.value}")
             }
         }
     }
@@ -77,7 +92,7 @@ class Board {
     fun printFriendlyBoard() {
         println("-------------------------------------------------------")
         rows.forEach { row ->
-            row.forEach { cell ->
+            row.cells.forEach { cell ->
                 print("|")
                 print(cell.value)
             }
@@ -86,25 +101,103 @@ class Board {
         }
 
     }
+
+    fun printPossibleValuesForPosition(position: Int) {
+        val rows = rows[position / 9]
+        val columns = columns[position % 9]
+        val boxes = boxes[((position / 3) % 3) + ((position / 27) * 3)]
+        boxes.cells.forEach {
+
+            println(it.value)
+        }
+    }
+
+    private fun setCellValue(position: Int, value: Value) {
+        cells[position].setValue(value)
+        rows[position / 9].updateValues(value)
+        columns[position % 9].updateValues(value)
+        // 0, 1, 2, 3, 4, 5, 6, 7, 8
+        // 0, 0, 0, 1, 1, 1, 2, 2, 2
+
+        // 9, 10, 11, 12, 13, 14, 15, 16, 17
+        // 0,  0,  0,  1,  1,  1,  2,  2,  2
+        boxes[((position / 3) % 3) + ((position / 27) * 3)].updateValues(value)
+    }
 }
 
-class Cell(var value: Value, val location: Int)
+interface ValueSetInterface {
+    val cells: Array<Cell>
+
+    val values: Set<Value>
+    val possibleValues: Set<Value>
+
+    fun updateValues(value: Value)
+}
+
+class ValueSet(override val cells: Array<Cell>) : ValueSetInterface {
+    private val _values = mutableSetOf<Value>()
+    override val values: Set<Value> = _values
+
+    override val possibleValues: Set<Value>
+        get() = allPossibleValues - values
+
+    override fun updateValues(value: Value) {
+        _values.add(value)
+    }
+
+}
+
+private val allPossibleValues = setOf(
+    Value.ONE,
+    Value.TWO,
+    Value.THREE,
+    Value.FOUR,
+    Value.FIVE,
+    Value.SIX,
+    Value.SEVEN,
+    Value.EIGHT,
+    Value.NINE
+)
+
+class Cell(val position: Int) {
+    var value: Value = Value.EMPTY
+        private set
+
+    fun setValue(value: Value) {
+        this.value = value
+    }
+}
 
 enum class Value {
     EMPTY, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE;
 
     override fun toString(): String {
         return when (this) {
-            EMPTY -> "EMPTY"
-            ONE -> "-ONE-"
-            TWO -> "-TWO-"
-            THREE -> "THREE"
-            FOUR -> "-FOUR"
-            FIVE -> "-FIVE"
-            SIX -> "-SIX-"
-            SEVEN -> "SEVEN"
-            EIGHT -> "EIGHT"
-            NINE -> "-NINE"
+            EMPTY -> "--.--"
+            ONE -> "--1--"
+            TWO -> "--2--"
+            THREE -> "--3--"
+            FOUR -> "--4--"
+            FIVE -> "--5--"
+            SIX -> "--6--"
+            SEVEN -> "--7--"
+            EIGHT -> "--8--"
+            NINE -> "--9--"
         }
+    }
+}
+
+private fun Char.toValue(): Value {
+    return when (this) {
+        '1' -> Value.ONE
+        '2' -> Value.TWO
+        '3' -> Value.THREE
+        '4' -> Value.FOUR
+        '5' -> Value.FIVE
+        '6' -> Value.SIX
+        '7' -> Value.SEVEN
+        '8' -> Value.EIGHT
+        '9' -> Value.NINE
+        else -> Value.EMPTY
     }
 }
--- Problem37/src/main/kotlin/Solution.kt
+++ Problem37/src/main/kotlin/Solution.kt
@@ -3,6 +3,19 @@ class Solution {
 }
 
 fun main() {
-    val board = Board()
+    val board = Board(board1)
     board.printFriendlyBoard()
+    board.printPossibleValuesForPosition(2)
 }
+
+val board1 = arrayOf(
+    charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+    charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+    charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+    charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+    charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+    charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+    charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+    charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+    charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+)
