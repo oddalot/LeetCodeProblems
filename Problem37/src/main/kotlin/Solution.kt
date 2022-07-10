fd554243b38d37b77981572d719a012893e54e95
oddalot <oddalot@yahoo.com>
2022-07-09 20:19:10 -0600

    add medium board, can not solve yet

--- Problem37/src/main/kotlin/Solution.kt
+++ Problem37/src/main/kotlin/Solution.kt
@@ -3,10 +3,11 @@ class Solution {
 }
 
 fun main() {
-    val board = Board(board1)
+    val board = Board(board1Easy)
 }
 
-val board1 = arrayOf(
+
+val board1Easy = arrayOf(
     charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
     charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
     charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
@@ -16,4 +17,16 @@ val board1 = arrayOf(
     charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
     charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
     charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+)
+
+val board2Medium = arrayOf(
+    charArrayOf('.', '7', '4', '.', '.', '.', '8', '5', '.'),
+    charArrayOf('.', '5', '.', '7', '.', '1', '.', '.', '.'),
+    charArrayOf('.', '.', '.', '.', '8', '5', '7', '.', '2'),
+    charArrayOf('.', '6', '.', '.', '.', '.', '2', '.', '.'),
+    charArrayOf('.', '3', '.', '5', '7', '.', '.', '9', '6'),
+    charArrayOf('.', '.', '1', '.', '.', '6', '5', '.', '4'),
+    charArrayOf('3', '.', '6', '.', '1', '.', '.', '.', '.'),
+    charArrayOf('.', '.', '.', '.', '6', '.', '.', '.', '.'),
+    charArrayOf('.', '.', '.', '8', '5', '4', '6', '1', '3')
 )
