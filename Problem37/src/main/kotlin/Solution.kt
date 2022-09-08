144e9f710e859e5697c40e7c5b419fb870f2a214
oddalot <oddalot@yahoo.com>
2022-09-07 19:32:31 -0600

    solve provlem 37

--- Problem37/src/main/kotlin/Solution.kt
+++ Problem37/src/main/kotlin/Solution.kt
@@ -1,33 +1,106 @@
 class Solution {
+    fun solveSudoku(board: Array<CharArray>): Unit {
+        solveSudokuHelper(board)
+    }
 
+    private fun solveSudokuHelper(board: Array<CharArray>): Boolean {
+        board.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { cellIndex, cell ->
+                if (cell == '.') {
+                    ('1'..'9').forEach { i ->
+                        board[rowIndex][cellIndex] = i
+                        if (isValidSudoku(board) && solveSudokuHelper(board)) {
+                            return true
+                        } else {
+                            board[rowIndex][cellIndex] = '.'
+                        }
+                    }
+
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    private fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val boxArrays = Array(9, { CharArray(9) { '.' } })
+        val columnArrays = Array(9, { CharArray(9) { '.' } })
+
+        board.forEachIndexed { rowIndex, charArray ->
+            val startIndex = when (rowIndex) {
+                0, 3, 6 -> 0
+                1, 4, 7 -> 3
+                2, 5, 8 -> 6
+                else -> throw IndexOutOfBoundsException(rowIndex.toString() + " not within index")
+            }
+
+            if (!isValidArray(charArray)) return false
+
+            charArray.forEachIndexed { columnIndex, char ->
+                val box = when (rowIndex) {
+                    0, 1, 2 -> {
+                        when (columnIndex) {
+                            0, 1, 2 -> 0
+                            3, 4, 5 -> 1
+                            6, 7, 8 -> 2
+                            else -> throw IndexOutOfBoundsException(columnIndex.toString() + " not within index")
+                        }
+                    }
+                    3, 4, 5 -> {
+                        when (columnIndex) {
+                            0, 1, 2 -> 3
+                            3, 4, 5 -> 4
+                            6, 7, 8 -> 5
+                            else -> throw IndexOutOfBoundsException(columnIndex.toString() + " not within index")
+                        }
+                    }
+                    6, 7, 8 -> {
+                        when (columnIndex) {
+                            0, 1, 2 -> 6
+                            3, 4, 5 -> 7
+                            6, 7, 8 -> 8
+                            else -> throw IndexOutOfBoundsException(columnIndex.toString() + " not within index")
+                        }
+                    }
+                    else -> throw IndexOutOfBoundsException(rowIndex.toString() + " not within index")
+                }
+
+                val boxIndex = startIndex + when (columnIndex) {
+                    0, 1, 2 -> columnIndex
+                    3, 4, 5 -> columnIndex - 3
+                    6, 7, 8 -> columnIndex - 6
+                    else -> throw IndexOutOfBoundsException(columnIndex.toString() + " not within index")
+                }
+
+                boxArrays[box][boxIndex] = char
+                columnArrays[columnIndex][rowIndex] = char
+            }
+        }
+
+        boxArrays.forEach {
+            if (!isValidArray(it)) return false
+        }
+
+        columnArrays.forEach {
+            if (!isValidArray(it)) return false
+        }
+
+        return true
+    }
+
+    private fun isValidArray(box: CharArray): Boolean {
+        val boxCheck = BooleanArray(9) { false }
+
+        box.forEach { char ->
+            val digit = Character.getNumericValue(char)
+            if (digit.toInt() <= 9 && digit.toInt() >= 1) {
+                if (boxCheck[digit - 1] == true) return false
+                boxCheck[digit - 1] = true
+            }
+        }
+
+        return true
+    }
 }
-
-fun main() {
-    val board = Board(board2Medium)
-}
-
-
-
-val board1Easy = arrayOf(
-    charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
-    charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
-    charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
-    charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
-    charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
-    charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
-    charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
-    charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
-    charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
-)
-
-val board2Medium = arrayOf(
-    charArrayOf('.', '7', '4', '.', '.', '.', '8', '5', '.'),
-    charArrayOf('.', '5', '.', '7', '.', '1', '.', '.', '.'),
-    charArrayOf('.', '.', '.', '.', '8', '5', '7', '.', '2'),
-    charArrayOf('.', '6', '.', '.', '.', '.', '2', '.', '.'),
-    charArrayOf('.', '3', '.', '5', '7', '.', '.', '9', '6'),
-    charArrayOf('.', '.', '1', '.', '.', '6', '5', '.', '4'),
-    charArrayOf('3', '.', '6', '.', '1', '.', '.', '.', '.'),
-    charArrayOf('.', '.', '.', '.', '6', '.', '.', '.', '.'),
-    charArrayOf('.', '.', '.', '8', '5', '4', '6', '1', '3')
-)
--- Problem37/src/test/kotlin/Test.kt
+++ Problem37/src/test/kotlin/Test.kt
@@ -1,3 +1,74 @@
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+
 class Test {
     private val solution = Solution()
+
+    @Test
+    fun `solves easy board`() {
+        val boardEasy = arrayOf(
+            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+        )
+
+        val boardEasySolved = arrayOf(
+            charArrayOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
+            charArrayOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
+            charArrayOf('1', '9', '8', '3', '4', '2', '5', '6', '7'),
+            charArrayOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
+            charArrayOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
+            charArrayOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
+            charArrayOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
+            charArrayOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
+            charArrayOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
+        )
+
+        solution.solveSudoku(boardEasy)
+
+        assertArrayEquals(boardEasySolved, boardEasy)
+    }
+
+    @Test
+    fun `solves medium board`() {
+        val boardMedium = arrayOf(
+            charArrayOf('.', '7', '4', '.', '.', '.', '8', '5', '.'),
+            charArrayOf('.', '5', '.', '7', '.', '1', '.', '.', '.'),
+            charArrayOf('.', '.', '.', '.', '8', '5', '7', '.', '2'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '.', '.'),
+            charArrayOf('.', '3', '.', '5', '7', '.', '.', '9', '6'),
+            charArrayOf('.', '.', '1', '.', '.', '6', '5', '.', '4'),
+            charArrayOf('3', '.', '6', '.', '1', '.', '.', '.', '.'),
+            charArrayOf('.', '.', '.', '.', '6', '.', '.', '.', '.'),
+            charArrayOf('.', '.', '.', '8', '5', '4', '6', '1', '3')
+        )
+
+        val boardMediumSolved = arrayOf(
+            charArrayOf('2', '7', '4', '6', '9', '3', '8', '5', '1'),
+            charArrayOf('6', '5', '8', '7', '2', '1', '3', '4', '9'),
+            charArrayOf('9', '1', '3', '4', '8', '5', '7', '6', '2'),
+            charArrayOf('5', '6', '7', '1', '4', '9', '2', '3', '8'),
+            charArrayOf('4', '3', '2', '5', '7', '8', '1', '9', '6'),
+            charArrayOf('8', '9', '1', '2', '3', '6', '5', '7', '4'),
+            charArrayOf('3', '8', '6', '9', '1', '7', '4', '2', '5'),
+            charArrayOf('1', '4', '5', '3', '6', '2', '9', '8', '7'),
+            charArrayOf('7', '2', '9', '8', '5', '4', '6', '1', '3')
+        )
+
+        solution.solveSudoku(boardMedium)
+
+        assertArrayEquals(boardMediumSolved, boardMedium)
+    }
 }
+
+
+
+
